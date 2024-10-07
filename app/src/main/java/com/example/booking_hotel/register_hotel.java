3749b9fd64eab09bf1741897e0e1370ddd045e5c
quockhanh <animelylia@gmail.com>
2024-10-07 11:38:45 +0700

    Updated 5 scenes
    xml code, login-registration-forgot password and change Vietnamese language

--- app/src/main/java/com/example/booking_hotel/register_hotel.java
+++ app/src/main/java/com/example/booking_hotel/register_hotel.java
@@ -1,6 +1,10 @@
 package com.example.booking_hotel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,33 @@ public class register_hotel extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register_hotel);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        Button btn = findViewById(R.id.btnRegister);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_register = new Intent(register_hotel.this , sign_in.class);
+                startActivity(intent_register);
+            }
+        });
+
+        TextView textView_haveAccount = findViewById(R.id.haveAccount);
+        textView_haveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  start_sign = new Intent(register_hotel.this , sign_in.class);
+                startActivity(start_sign);
+            }
+        });
+
+        TextView khanh_404 = findViewById(R.id.Khanh_2725);
+        khanh_404.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  Khanh2725 = new Intent(register_hotel.this , register_hotel_1.class);
+                startActivity(Khanh2725);
+            }
         });
     }
 }
--- app/src/main/java/com/example/booking_hotel/sign_in.java
+++ app/src/main/java/com/example/booking_hotel/sign_in.java
@@ -1,6 +1,10 @@
 package com.example.booking_hotel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,24 @@ public class sign_in extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        TextView textView_register = findViewById(R.id.textViewRegister);
+        textView_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  sign_register = new Intent(sign_in.this , register_hotel.class);
+                startActivity(sign_register);
+            }
+        });
+
+        TextView textView_forgot = findViewById(R.id.forgotPass);
+        textView_forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent forgot_pass = new Intent(sign_in.this, forgot_pass.class);
+                startActivity(forgot_pass);
+            }
         });
     }
 }
