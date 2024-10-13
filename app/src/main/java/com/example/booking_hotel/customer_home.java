de36624ac22906ed2c17fa401c2f55aa883cde49
quockhanh <animelylia@gmail.com>
2024-10-13 13:25:21 +0700

    up1

--- app/src/main/java/com/example/booking_hotel/customer_home.java
+++ app/src/main/java/com/example/booking_hotel/customer_home.java
@@ -7,18 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class customer_home extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+    DrawerLayout dra ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_customer_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
     }
 }
