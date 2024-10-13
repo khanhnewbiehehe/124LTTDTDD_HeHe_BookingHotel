af0a3e6fc8f32faf2c8379ea28bc19a5a4d4dd2a
quockhanh <animelylia@gmail.com>
2024-10-13 12:05:06 +0700

    up

---
+++ app/src/main/java/com/example/booking_hotel/img_slider.java
@@ -0,0 +1,17 @@
+package com.example.booking_hotel;
+
+public class img_slider {
+    private int resourceId;
+
+    public img_slider(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+}
