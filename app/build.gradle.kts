d5c1803d3f4cff0084650a8234d26998e2a9a20d
NguyenAnhHoangIT <nahoangit@gmail.com>
2024-10-13 13:09:58 +0700

    room detail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.tools.r8.internal.me
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -40,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("me.relex:circleindicator:2.1.6")
 }
