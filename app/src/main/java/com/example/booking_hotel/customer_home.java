cc4abf6b30f8cf030010fe7edfa8739881e51765
quockhanh <animelylia@gmail.com>
2024-10-15 00:18:26 +0700

    nav_menu

--- app/src/main/java/com/example/booking_hotel/customer_home.java
+++ app/src/main/java/com/example/booking_hotel/customer_home.java
@@ -1,16 +1,22 @@
 package com.example.booking_hotel;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -20,6 +26,9 @@ public class customer_home extends AppCompatActivity {
     FloatingActionButton fab;
 
     DrawerLayout dra ;
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
 
 
@@ -40,5 +49,23 @@ public class customer_home extends AppCompatActivity {
             }
         });
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_logout) {
+                    Log.d("NavigationView", "Đăng xuất được nhấn");
+                    Intent intent = new Intent(customer_home.this, sign_in.class);
+                    startActivity(intent);
+                    finish();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
 }
