a6393e034d360c0c1c546dd81e95223631c36b29
NguyenNhatKhanh <nguyennhatkhanh151203@gmail.com>
2024-12-03 17:21:59 +0700

    Quan ly loai phong

---
+++ HotelManage_LTDD/HotelManage_LTDD/Models/RoomType.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelManage_LTDD.Models
+{
+    public class RoomType
+    {
+        public string? Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public int People { get; set; }
+    }
+}
